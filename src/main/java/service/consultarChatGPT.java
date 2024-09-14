0c469460e195b0f060058daa4f31003a296e51f7
Juliano Ambrósio <gestaojuliano99@gmail.com>
2024-09-14 17:16:32 -0300

    first commit

---
+++ src/main/java/service/consultarChatGPT.java
@@ -0,0 +1,19 @@
+package service;
+
+public class ConsultaChatGPT {
+    public static String obterTraducao(String texto) {
+
+        OpenAiService service = new OpenAiService("sk-proj-
+                qVUKVbaZzSEZxGJcj9ART3BlbkFJdNb3JBiImy2fpF1fy41o");
+
+// chave expirou. Cadastre-se e solicite uma nova chave
+                CompletionRequest requisicao = CompletionRequest.builder()
+                .model("gpt-3.5-turbo-instruct")
+                .prompt("traduza para o português o texto: " + texto)
+                .maxTokens(1000)
+                .temperature(0.7)
+                .build();
+        var resposta = service.createCompletion(requisicao);
+        return resposta.getChoices().get(0).getText();
+    }
+}
